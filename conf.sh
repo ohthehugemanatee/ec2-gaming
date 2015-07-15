670b7f930ba4a457d3f2204501aa3b8762dc4d25
Campbell Vertesi <campbell@vertesi.com>
2015-07-15 19:03:41 +0200

    sample conf.sh file

---
+++ conf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configuration for your EC2 gaming instance. Fill in all these values.
+
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=
+export EC2_DEFAULT_ZONE=
+export EC2_SECURITY_GROUP_ID=
